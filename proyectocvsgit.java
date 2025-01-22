af341d5ac00fee707b54bdf710df412466b6c84b
Luisj69m <luisjosemarcanofuentes@gmail.com>
2025-01-22 17:30:52 +0100

    Hola mundo
    todo lo que se necesita...

--- proyectocvsgit.java
+++ proyectocvsgit.java
@@ -1,6 +1,6 @@
 public class proyectocvsgit {
     public static void main(String[] args) {
-        System.out.println("Hola mundo cruel");
+        System.out.println("Hola mundo super guay.");
 
 
     }
