31f9d1cc8681a67c748b89010cc305250ff4d4a1
Luisj69m <luisjosemarcanofuentes@gmail.com>
2025-01-22 17:48:32 +0100

    Hola que hace
    Todo lo cambios se hacen...

--- proyectocvsgit.java
+++ proyectocvsgit.java
@@ -1,6 +1,6 @@
 public class proyectocvsgit {
     public static void main(String[] args) {
-        System.out.println("Hola mundo super guay.");
+        System.out.println("Hola mundo super guay.hola que hace");
 
 
     }
